9316d3888e8d411893f0326ad1f8eb2e1d93e9e3
James Basile <jbasile14@gmail.com>
2019-01-31 17:55:27 -0500

    got rid of unneccesary 'Array.from' in the first 'select()' method

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -30,7 +30,7 @@ class TabLink {
 
     // Using a loop or the forEach method remove the 'tabs-link-selected'
     // class from all of the links
-    Array.from(links).forEach( x => {
+    links.forEach( x => {
       //console.log(x);
       x.classList.remove('tabs-link-selected');
     });
